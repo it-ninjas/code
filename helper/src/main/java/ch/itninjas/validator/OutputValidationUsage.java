fd95149bb7ab063348549b98bf4405962c302171
Jonas Schmid <schmid@puzzle.ch>
2023-06-16 09:08:26 +0200

    improve OutputValidationUsage

--- helper/src/main/java/ch/itninjas/validator/OutputValidationUsage.java
+++ helper/src/main/java/ch/itninjas/validator/OutputValidationUsage.java
@@ -5,24 +5,31 @@ public class OutputValidationUsage {
     private static final OutputValidation outputValidation = new OutputValidation();
 
     public static void main(String[] args) {
-        outputValidation.logAndPrint("Programm is starting ...");
+        outputValidation.logAndPrint("1) Programm is starting ...");
         OutputValidationUsage outputValidationUsage = new OutputValidationUsage();
         outputValidationUsage.verifyOutput();
     }
 
     public OutputValidationUsage() {
-        outputValidation.logAndPrint("Start working");
-        outputValidation.log("This is not a interesting line. Do not print to console.");
-        outputValidation.logAndPrint("I work very hard!");
-        outputValidation.logAndPrint("I'm finish.");
+        outputValidation.logAndPrint("2) Start working");
+        outputValidation.log("3) This is not a interesting line. Only log this line.");
+        outputValidation.logAndPrint("4) I work very hard!");
+        outputValidation.logAndPrint("5) I'm finish.");
     }
 
     public void verifyOutput() {
         // print the saved console output
+        System.out.println("--------------------------------------------------");
+        System.out.println("--> Print all logged outputs: ");
         outputValidation.printAll();
+        System.out.println("--------------------------------------------------");
         // print the hash code
+        System.out.print("--> The control hash is: ");
         outputValidation.printControlHash();
         // is the validation ok
+        System.out.print("--> Is the control 2128163011 correct: ");
         System.out.println(outputValidation.verifyControlHash(2128163011));
+        System.out.print("--> Is the control -105527360 correct: ");
+        System.out.println(outputValidation.verifyControlHash(-105527360));
     }
 }
